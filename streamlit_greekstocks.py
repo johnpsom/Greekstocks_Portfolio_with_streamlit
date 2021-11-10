0081ff0255509dfaf75830c1662e0000e38d2869
johnpsom <52068616+johnpsom@users.noreply.github.com>
2021-11-10 13:35:30 +0200

    Update streamlit_greekstocks.py

--- streamlit_greekstocks.py
+++ streamlit_greekstocks.py
@@ -122,8 +122,8 @@ def cumulative_returns(stock,returns):
 def send_portfolio_byemail(filename, receiver_email):
     smtp_server = "smtp.gmail.com"
     port =465 # For starttls
-    sender_email = "getyour.portfolio@gmail.com"
-    password = "!@#portfolio20"
+    sender_email = sender_email
+    password = password
     subject = "Το Χαρτοφυλάκιό σου"
     body = "Βρες στο συνημμένο αρχείο το χαρτοφυλάκιο που έχεις φτιάξει."
     # Create a multipart message and set headers
